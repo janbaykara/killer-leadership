bbae7926b91920845f2d434672c58ea2a43b4e4b
Jan Baykara <janbaykara@gmail.com>
2014-09-21 02:52:05 +0100

    Initial commit, basic data calcs & display, no styling.

--- index.php
+++ index.php
@@ -2,13 +2,28 @@
 require(dirname(__FILE__)."/php/config.php");
 
 $app = new App();
-$app->HTMLINJECT = "ng-app";
+$app->HTMLINJECT = "ng-app='app'";
 $app->BODYINJECT = null;
 $app->PAGETITLE = "KAKAW";
 
 $app->SCAFFOLD_HEAD();
 ?>
-<main id="wrapper">
-Hello world.
+<main id="wrapper" ng-controller="GraphController">
+		<header class="row" id="timer">
+			<h1>If the top 10 most terrible leaders in the history of the world were still at it today,
+			in {{timeElapsed}} seconds they would have taken care of a collective {{combinedKillCount.toFixed(0)}} helpless plebs.</h1>
+		</div>
+		<ol class="row">
+			<li class="ruler column small-12" ng-repeat="ruler in data.rulers">
+				<div class="ruler-info">
+					<h3>{{ruler.name}}</h3>
+					<div class="ruler-desc">{{ruler.kills}} in {{ruler.tenure}} years means {{ruler.kps.toFixed(4)}} kills per second.</div>
+				</div>
+				<div class="ruler-counter">
+					{{ruler.name}} would have killed {{ruler.killCount.toFixed(2)}} since you loaded this page...
+				</div>
+			</li>
+		</ol>
+	</div>
 </main>
 <? $app->SCAFFOLD_FOOT() ?>
--- js/app.js
+++ js/app.js
@@ -2,6 +2,59 @@
 
 var app = angular.module("app", []);
 
-app.controller('formController', function($scope) {
-	// ...
+app.controller('GraphController', function($scope,$interval,DataService,CurrentTimeService) {
+	// Load data from service
+    var DataPromise = DataService.getData($scope.topicName);
+
+    // Wait for the data to arrive
+    DataPromise.then(function(data) {
+    	// Calculate the statistical analytics
+    	$scope.data = data;
+
+		angular.forEach($scope.data.rulers, function(ruler) {
+			ruler.tenure = ruler.to - ruler.from;
+			ruler.kps = ruler.kills / (ruler.tenure * 365 * 24 * 60 * 60);
+		});
+    });
+
+	// Update killcount per second
+	var delay = 1000; // 1 sec
+	$scope.timeElapsed = 0;
+	$scope.combinedKillCount = 0;
+
+	$interval(function() {
+		$scope.combinedKillCount = 0;
+		$scope.timeElapsed++;
+		// The meat of it.
+		angular.forEach($scope.data.rulers, function(ruler) {
+			ruler.killCount = ruler.kps * $scope.timeElapsed;
+			$scope.combinedKillCount += ruler.killCount;
+		})
+    }, delay);
+})
+
+app.factory('CurrentTimeService', function(){
+	return { 
+		get: function() {
+			return new Date();
+		}
+	}; 
+});
+
+app.factory('DataService', function($http, $q) {
+
+    var getData = function(request) {
+        var deferred = $q.defer();
+
+		$http.get('js/data.json')
+		.success(function(data) {
+            deferred.resolve(data);
+		})
+		.error(function(data) {
+			deferred.resolve("Oops, couldn't find the data. Try again :)");
+		});
+
+        return deferred.promise;
+    };
+    return { getData: getData };
 });
